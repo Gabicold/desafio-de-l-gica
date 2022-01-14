5b9f29ba12e01d707a2a2c93812c7b69124de640
Gabicold <biel18102004@gmail.com>
2022-01-14 18:44:34 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = "Gabriel"
+let numero = 10
+let ano = 2022
+console.log(numero - ano)
+console.log("Estou mexendo no console.log")
+let quartaFeira = false
+console.log(`O valor boleano é ${quartaFeira}`)
+console.log(quartaFeira)
+console.log("Boolean")
